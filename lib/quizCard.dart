fcae767b52fce16a876b027a5cdd323f3afb8b4a
SG <sgng1@e.ntu.edu.sg>
2019-12-09 09:51:42 +0000

    retrieved info from database directly

--- lib/quizCard.dart
+++ lib/quizCard.dart
@@ -1,36 +1,45 @@
 import 'package:flutter/material.dart';
 import 'config.dart';
 import 'quizPage.dart';
+import 'package:provider/provider.dart';
+import 'main.dart';
+import 'package:firebase/firestore.dart';
 
 class QuizCardContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Flexible(
-        child: GridView.builder(
-            padding: EdgeInsets.all(16.0),
-            itemCount: config['mockQuiz'].length,
-            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: config['cardMaxWidth'],
-                childAspectRatio: 1.5),
-            itemBuilder: (BuildContext context, int index) {
-              final quizInfo = config['mockQuiz'][index];
-              return _buildQuizCards(quizInfo, context);
+        child: StreamBuilder<QuerySnapshot>(
+            stream: Provider.of<Fs>(context, listen: false)
+                .getStore
+                .collection('testQuiz')
+                .onSnapshot,
+            builder: (BuildContext context,
+                AsyncSnapshot<QuerySnapshot> querySnapshot) {
+              if (!querySnapshot.hasData) return LinearProgressIndicator();
+              return GridView.builder(
+                  padding: EdgeInsets.all(16.0),
+                  itemCount: querySnapshot.data.docs.length,
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: config['cardMaxWidth'],
+                      childAspectRatio: 1.5),
+                  itemBuilder: (BuildContext context, int index) {
+                    Map quizInfo = querySnapshot.data.docs[index].data();
+                    quizInfo['id'] = querySnapshot.data.docs[index].id;
+                    return Card(
+                        child: InkWell(
+                            onTap: () {
+                              var linkFirestore = Provider.of<Fs>(context);
+                              Navigator.of(context).push(
+                                  MaterialPageRoute<void>(
+                                      builder: (BuildContext context) {
+                                return ChangeNotifierProvider.value(
+                                    value: linkFirestore,
+                                    child: QuizPage(quizInfo: quizInfo));
+                              }));
+                            },
+                            child: ListTile(title: Text(quizInfo['title']))));
+                  });
             }));
-  }
-
-  Widget _buildQuizCards(quizInfo, context) {
-    void _openQuiz(quizInfo) {
-      Navigator.of(context)
-          .push(MaterialPageRoute<void>(builder: (BuildContext context) {
-        return QuizPage(quizInfo: quizInfo);
-      }));
-    }
-
-    return Card(
-        child: InkWell(
-            onTap: () {
-              _openQuiz(quizInfo);
-            },
-            child: ListTile(title: Text(quizInfo['quizTitle']))));
   }
 }
--- lib/quizPage.dart
+++ lib/quizPage.dart
@@ -4,6 +4,8 @@ import 'quizQn.dart';
 import 'resultCalc.dart';
 import 'showAlertDialog.dart';
 import 'package:provider/provider.dart';
+import 'main.dart';
+import 'package:firebase/firestore.dart';
 
 class QuizAnswers with ChangeNotifier {
   Map _quizScore = {};
@@ -23,60 +25,61 @@ class QuizPage extends StatelessWidget {
   const QuizPage({Key key, this.quizInfo}) : super(key: key);
   final Map quizInfo;
 
-  List<Map<dynamic, dynamic>> getQuizQuestions() => quizInfo['questions'];
-
-  String getTypeFromQn(question) {
-    String interimValue = '';
-    for (Map quizQn in getQuizQuestions()) {
-      if (quizQn['title'] == question) {
-        interimValue = quizQn['type'];
-      }
-    }
-    return interimValue;
-  }
-
-  String getQuizTitle() => quizInfo['quizTitle'];
+  String getQuizTitle() => quizInfo['title'];
 
   Widget quizQnContainer(context) {
-    return ListView.builder(
-        physics: AlwaysScrollableScrollPhysics(),
-        padding: EdgeInsets.all(16.0),
-        itemCount: getQuizQuestions().length,
-        itemBuilder: (BuildContext context, int index) {
-          final quizDetails = {
-            'title': getQuizQuestions()[index]['title'],
-            'type': getQuizQuestions()[index]['type'],
-            'index': index
-          };
-          Widget quizQuestion = QuizQn(
-              key: UniqueKey(),
-              quizDetails: quizDetails,
-              updateQuizScore: Provider.of<QuizAnswers>(context, listen: false)
-                  ._updateQuizScore);
+    return StreamBuilder<QuerySnapshot>(
+        stream: Provider.of<Fs>(context, listen: false)
+            .getStore
+            .collection('testQuestions')
+            .where('quiz', 'array-contains', quizInfo['id'])
+            .onSnapshot,
+        builder:
+            (BuildContext context, AsyncSnapshot<QuerySnapshot> querySnapshot) {
+          if (!querySnapshot.hasData) return LinearProgressIndicator();
 
-          if (index == getQuizQuestions().length - 1) {
-            return Column(
-              children: <Widget>[
-                quizQuestion,
-                Consumer<QuizAnswers>(
-                  builder: (context, quizAnswers, child) => RaisedButton(
-                    onPressed: () {
-                      String results = 'Your result is ' +
-                          calculateResults(
-                              quizAnswers.getQuizScore)['outcome'] +
-                          ' and your scores are ' +
-                          calculateResults(quizAnswers.getQuizScore)['scores']
-                              .toString();
-                      showAlertDialog(context, results);
-                    },
-                    child: Text('Submit', style: TextStyle(fontSize: 20)),
-                  ),
-                )
-              ],
-            );
-          } else {
-            return quizQuestion;
-          }
+          return ListView.builder(
+              physics: AlwaysScrollableScrollPhysics(),
+              padding: EdgeInsets.all(16.0),
+              itemCount: querySnapshot.data.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                final quizDetails = {
+                  'title': querySnapshot.data.docs[index].data()['title'],
+                  'type': querySnapshot.data.docs[index].data()['type'],
+                  'index': index
+                };
+                Widget quizQuestion = QuizQn(
+                    key: UniqueKey(),
+                    quizDetails: quizDetails,
+                    updateQuizScore:
+                        Provider.of<QuizAnswers>(context, listen: false)
+                            ._updateQuizScore);
+
+                if (index == querySnapshot.data.docs.length - 1) {
+                  return Column(
+                    children: <Widget>[
+                      quizQuestion,
+                      Consumer<QuizAnswers>(
+                        builder: (context, quizAnswers, child) => RaisedButton(
+                          onPressed: () {
+                            String results = 'Your result is ' +
+                                calculateResults(
+                                    quizAnswers.getQuizScore)['outcome'] +
+                                ' and your scores are ' +
+                                calculateResults(
+                                        quizAnswers.getQuizScore)['scores']
+                                    .toString();
+                            showAlertDialog(context, results);
+                          },
+                          child: Text('Submit', style: TextStyle(fontSize: 20)),
+                        ),
+                      )
+                    ],
+                  );
+                } else {
+                  return quizQuestion;
+                }
+              });
         });
   }
 
