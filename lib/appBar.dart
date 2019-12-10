fff4e8f4523564b656faf5f3cb239bbbaa5a65b4
SG <sgng1@e.ntu.edu.sg>
2019-12-10 05:42:05 +0000

    added button that triggers google auth

--- lib/appBar.dart
+++ lib/appBar.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:firebase/firebase.dart' as fb;
 
 Widget myAppBar([String title = 'Quiz App']) => AppBar(
-      title: FittedBox(fit: BoxFit.fitWidth, child: Text(title)),
-    );
+        title: FittedBox(fit: BoxFit.fitWidth, child: Text(title)),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Site\n Admin',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () async {
+              var fbAuth = fb.auth();
+              var provider = fb.GoogleAuthProvider();
+              try {
+                await fbAuth.signInWithPopup(provider);
+              } catch (e) {
+                print("Error in sign in with google: $e");
+              }
+            },
+          ),
+        ]);
