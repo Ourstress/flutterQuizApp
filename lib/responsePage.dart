968e056ef190bdb062608c954b51aa4924992151
SG <sgng1@e.ntu.edu.sg>
2019-12-31 05:55:08 +0000

    display quiz result outcome in pie chart

--- lib/responsePage.dart
+++ lib/responsePage.dart
@@ -10,35 +10,45 @@ class ResponsePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(processQuizInfo().tabulateResponses);
     return Scaffold(
         appBar: myAppBar('Responses'),
-        body: charts.PieChart(_createSampleData(), animate: false));
+        body: charts.PieChart(_createSampleData(),
+            animate: false,
+            defaultRenderer: new charts.ArcRendererConfig(
+                arcWidth: 60,
+                arcRendererDecorators: [charts.ArcLabelDecorator()])));
   }
 
-  List<charts.Series<LinearSales, int>> _createSampleData() {
-    final data = [
-      LinearSales(0, 100),
-      LinearSales(1, 75),
-      LinearSales(2, 25),
-      LinearSales(3, 5),
-    ];
+  List<charts.Series<TabulatedResponse, String>> _createSampleData() {
+    final data = processQuizInfo().tabulateResponses;
 
     return [
-      charts.Series<LinearSales, int>(
+      charts.Series<TabulatedResponse, String>(
         id: 'Sales',
-        domainFn: (LinearSales sales, _) => sales.year,
-        measureFn: (LinearSales sales, _) => sales.sales,
+        domainFn: (TabulatedResponse results, _) => results.type,
+        measureFn: (TabulatedResponse results, _) => results.count,
         data: data,
+        labelAccessorFn: (TabulatedResponse results, _) =>
+            '${results.type}: ${results.count}',
       )
     ];
   }
 }
 
-class LinearSales {
-  final int year;
-  final int sales;
+class QuizResponse {
+  final String gender;
+  final Map results;
+  final DateTime createdAt;
 
-  LinearSales(this.year, this.sales);
+  QuizResponse(this.gender, this.results, this.createdAt);
+}
+
+class TabulatedResponse {
+  final String type;
+  final int count;
+
+  TabulatedResponse(this.type, this.count);
 }
 
 class ProcessQuiz {
@@ -46,5 +56,24 @@ class ProcessQuiz {
 
   ProcessQuiz(this.quizInfo);
 
-  Map get quizResponses => quizInfo['responses'];
+  List<QuizResponse> get quizResponses => quizInfo['responses']
+      .values
+      .map<QuizResponse>((elem) =>
+          QuizResponse(elem['gender'], elem['results'], elem['createdAt']))
+      .toList();
+
+  List<TabulatedResponse> get tabulateResponses {
+    Map tabulatedScores = {};
+    for (QuizResponse response in quizResponses) {
+      Map results = response.results;
+      String answerType = results['outcome'];
+      if (!tabulatedScores.containsKey(answerType)) {
+        tabulatedScores[answerType] = 0;
+      }
+      tabulatedScores[answerType] += 1;
+    }
+    return tabulatedScores.keys
+        .map((key) => TabulatedResponse(key, tabulatedScores[key]))
+        .toList();
+  }
 }
